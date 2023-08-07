926863d8ebb8f0c9340acb804b5cf4e78daa3607
Thiago Carvalho da Silva <thi89248@gmail.com>
2023-08-07 08:03:09 -0300

    Cleaning up code and function divisions

--- src/spreadsheet_generate.py
+++ src/spreadsheet_generate.py
@@ -1,72 +1,31 @@
-#Biblioteca para lidar com dados JSON
 import json
-#Bibliotexa para a criação e manipulação de planilhas no Excel
 from openpyxl import Workbook
 
+def cells(vehicle):
+    ...
 
-file_excel = Workbook() #Cria um arquivo excel em branco 
-spreadsheet = file_excel.active #Acesso a planilha recém criada
-spreadsheet.title = 'Title' #Título da tabela
 
-# Abrir o arquivo JSON em modo de leitura e amazenar os dados na vairável 'file'
+file_excel = Workbook() 
+spreadsheet = file_excel.active 
+spreadsheet.title = 'REPORT.X'
+
+
 with open('data.json', 'r') as file:
-    object = json.load(file) # String JSON para objeto
-
-linha = 1
-coluna = 1
-vPlacaAtual = ""
-vTipoAtual = ""
-vTipoAtualFor = ""
-vPlacaAtualFor = ""
-
-#Cabeçalho da planilha
-spreadsheet.cell(row=1, column=1, value="Day")
-spreadsheet.cell(row=1, column=2, value="Plate")
-spreadsheet.cell(row=1, column=3, value="Vehicle")
-spreadsheet.cell(row=1, column=4, value="Day Period")
+    object = json.load(file)
 
 
-for k in object['rows']:    
-    
-    if (vTipoAtual == ""):
-        vTipoAtualFor = k["vTipo"]
-    else :
-        vTipoAtualFor = vTipoAtual
+line = 1
 
-    if (vPlacaAtual == ""):
-        vPlacaAtualFor = k["vPlaca"]
-    else :
-        vPlacaAtualFor = vPlacaAtual        
 
-    vTipoAtual = k["vTipo"]
+spreadsheet.cell(row = line, column = 1, value = 'Daty')
+spreadsheet.cell(row = line, column = 2, value = 'Plate')
+spreadsheet.cell(row = line, column = 3, value = 'Description Vehicle')
+spreadsheet.cell(row = line, column = 4, value = 'Day Period')
+spreadsheet.cell(row = line, column = 4, value = 'Weight')
 
-    if (vPlacaAtual != k["vPlaca"]):        		
-        vPlacaAtual = k["vPlaca"]	         
-        vTipoAtual = k["vTipo"]
-        
-        linha = linha + 1 
-        
-        spreadsheet.cell(row=linha, column=1, value=k["dia"])
-        spreadsheet.cell(row=linha, column=2, value=k["vPlaca"])
-        spreadsheet.cell(row=linha, column=3, value=k["vDescricao_veiculo"])
-        spreadsheet.cell(row=linha, column=4, value=k["vTipo"])   
-        coluna = 5     
- 
-    if (vTipoAtualFor != vTipoAtual) and (vPlacaAtualFor == vPlacaAtual):
-        linha = linha + 1
 
-        spreadsheet.cell(row=linha, column=1, value=k["dia"])
-        spreadsheet.cell(row=linha, column=2, value=k["vPlaca"])
-        spreadsheet.cell(row=linha, column=3, value=k["vDescricao_veiculo"])
-        spreadsheet.cell(row=linha, column=4, value=k["vTipo"])     
-
-        coluna = 5       
-
-    spreadsheet.cell(row=1, column=coluna, value="Peso")  
-    spreadsheet.cell(row=linha, column=coluna, value=k["nPeso"])  
-    coluna = coluna + 1
-
-    print(vPlacaAtualFor, vPlacaAtual, "-" ,vTipoAtualFor, vTipoAtual)
+for vehicle in object['rows']:    
+    cells(vehicle) 
 
 
 print(file_excel.sheetnames)
