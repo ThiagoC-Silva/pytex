ec6ac73f5c3b9bd57d12c71e3ed707689a15fb91
Thiago Carvalho da Silva <thi89248@gmail.com>
2023-08-07 08:55:53 -0300

    Correction of errors and completion of the base project

--- src/spreadsheet_generate.py
+++ src/spreadsheet_generate.py
@@ -4,6 +4,7 @@ from openpyxl import Workbook
 def cells(vehicle):
     status = False
     match_line = 1
+    global line
     for record in registered_vehicles:
         match_line += 1
         if(
@@ -11,33 +12,37 @@ def cells(vehicle):
             vehicle['Plate'] == record['Plate'] and
             vehicle['Day Period'] == record['Day Period']
         ):
-            weight_column = len(registered_vehicles)
+            
+            status = True
+            weight_column = len(record) + 1
             spreadsheet.cell(row = 1, column = weight_column, value = 'Weight' )
             spreadsheet.cell(row = match_line, column = weight_column, value = vehicle['Weight'] )
-            status = True
             line -= 1
+            break
         
-        if not status:
-            spreadsheet.cell(row  = line, column = 1, value = vehicle['Day'])
-            spreadsheet.cell(row  = line, column = 2, value = vehicle['Plate'])
-            spreadsheet.cell(row  = line, column = 3, value = vehicle['Description Vehicle'])
-            spreadsheet.cell(row  = line, column = 4, value = vehicle['Day Period'])
-            spreadsheet.cell(row  = line, column = 5, value = vehicle['Weight'])
-            
+    if status == False:
+        spreadsheet.cell(row  = line, column = 1, value = vehicle['Day'])
+        spreadsheet.cell(row  = line, column = 2, value = vehicle['Plate'])
+        spreadsheet.cell(row  = line, column = 3, value = vehicle['Description Vehicle'])
+        spreadsheet.cell(row  = line, column = 4, value = vehicle['Day Period'])
+        spreadsheet.cell(row  = line, column = 5, value = vehicle['Weight'])
+        
+
+    registered_vehicles.append(vehicle)
 
 file_excel = Workbook() 
 spreadsheet = file_excel.active 
 spreadsheet.title = 'REPORT.X'
 
 
-with open('data.json', 'r') as file:
+with open('src/data.json', 'r') as file:
     object = json.load(file)
 
 
 line = 1
 registered_vehicles = []
 
-spreadsheet.cell(row = line, column = 1, value = 'Daty')
+spreadsheet.cell(row = line, column = 1, value = 'Day')
 spreadsheet.cell(row = line, column = 2, value = 'Plate')
 spreadsheet.cell(row = line, column = 3, value = 'Description Vehicle')
 spreadsheet.cell(row = line, column = 4, value = 'Day Period')
@@ -45,7 +50,8 @@ spreadsheet.cell(row = line, column = 4, value = 'Weight')
 
 
 for vehicle in object['rows']:    
-    cells(vehicle) 
+    line += 1
+    cells(vehicle)
 
 
 print(file_excel.sheetnames)
