3e892cb91c7e31a1f98a4152bb3f8367d0f4c585
Thiago Carvalho da Silva <thi89248@gmail.com>
2023-08-15 20:22:16 -0300

    Cell update test

--- test_workbook.py
+++ test_workbook.py
@@ -16,9 +16,16 @@ def test_create_header(test_workbook):
 def test_insertion_cells_no_status(test_workbook):
     data_test={"Day": 1, "Plate": "ABC-1231", "Description Vehicle": "CAM. COMPAC. VOLKS", "Day Period": "DIURNO", "Weight": 2.49}
     test_workbook.insertion_cells(data_test, False, 2)
+    
     for col, data_value in enumerate(data_test.values(), start = 1):
         assert test_workbook.worksheet.cell(row = 2, column = col).value == data_value
 
 
 def test_insertion_cells_status(test_workbook):
-    pass
+    initial_columns = test_workbook.worksheet.max_column
+    data_test={"Day": 1, "Plate": "ABC-1231", "Description Vehicle": "CAM. COMPAC. VOLKS", "Day Period": "DIURNO", "Weight": 2.49}
+    test_workbook.insertion_cells(data_test, True, 2)
+
+    update_column = test_workbook.worksheet.max_column 
+    assert update_column == initial_columns + 1
+    assert test_workbook.worksheet.cell(row = 2, column = update_column).value == data_test['Weight']
