55c2989f33bae34919309e49d113a28155ef469f
Thiago Carvalho da Silva <thi89248@gmail.com>
2023-08-15 19:44:56 -0300

    New cell insertion test

--- test_workbook.py
+++ test_workbook.py
@@ -11,3 +11,14 @@ def test_create_header(test_workbook):
     header = test_workbook.header
     for col, header_value in enumerate(header, start = 1):
         assert test_workbook.worksheet.cell(row = 1, column = col).value == header_value
+
+
+def test_insertion_cells_no_status(test_workbook):
+    data_test={"Day": 1, "Plate": "ABC-1231", "Description Vehicle": "CAM. COMPAC. VOLKS", "Day Period": "DIURNO", "Weight": 2.49}
+    test_workbook.insertion_cells(data_test, False, 2)
+    for col, data_value in enumerate(data_test.values(), start = 1):
+        assert test_workbook.worksheet.cell(row = 2, column = col).value == data_value
+
+
+def test_insertion_cells_status(test_workbook):
+    pass
